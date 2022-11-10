d4d5a894e85a3c78020a912d4f386f8b4f9c7241
akeemahmid <113114055+akeemahmid@users.noreply.github.com>
2022-11-10 14:24:50 +0100

    Update cart.js

--- cart.js
+++ cart.js
@@ -10,11 +10,11 @@ let menu = document.querySelector('#menu')
 
 
 iconClick.onclick = () =>{
-    if (navMenu.style.right === '-250px'){
+    if (navMenu.style.right === '-400px'){
         navMenu.style.right = '0'
         menu.src = "./images/close.png"
     }else{
-        navMenu.style.right = '-250px'
+        navMenu.style.right = '-400px'
         menu.src = "./images/menu.png"
     }
 }
