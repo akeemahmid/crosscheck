3bd4a485ed5145af892f7035333330eb1317126a
akeemahmid <113114055+akeemahmid@users.noreply.github.com>
2022-11-10 14:13:40 +0100

    Update about.js

--- about.js
+++ about.js
@@ -3,11 +3,11 @@ let iconClick = document.querySelector('#icon')
 let menu = document.querySelector('#menu')
 
 iconClick.onclick = () =>{
-    if (navMenu.style.right === '-400px'){
+    if (navMenu.style.right === '-250px'){
         navMenu.style.right = '0'
         menu.src = "./images/close.png"
     }else{
-        navMenu.style.right = '-400px'
+        navMenu.style.right = '-250px'
         menu.src = "./images/menu.png"
     }
 }
